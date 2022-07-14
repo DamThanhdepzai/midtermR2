7d7f0712608a39ebee73207991e76d4cf16cac5b
DamThanhdepzai <haxom747@gmail.com>
2022-07-14 11:34:21 +0700

    hmmm

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -30,75 +30,7 @@ const FoodSearch = () => {
   }, []);
   return (
     <>
-      {/* <div>
-        <div>
-          <label htmlFor="search">Name</label>
-          <br />
-          <input
-            onChange={(e) => setSearch(e.target.value)}
-            type="text"
-            id="search"
-            name="search"
-          />
-          <br />
-          <label htmlFor="price">Price</label>
-          <br />
-          <input
-            onChange={(e) => setSearch(e.target.value)}
-            type="number"
-            id="search"
-            name="search"
-          />
-          <br />
-          <br />
-          <button
-            className="btn btn-success"
-            onClick={(e) => handleSearch(e, search)}
-            type="submit"
-          >
-            Search
-          </button>
-        </div>
-        <h1>
-          -------------------------------------------------Menu--------------------------------------
-        </h1>
-        {!!food ? (
-          food.map((food, index) => (
-            <div key={index} className="food">
-              <div>
-                <a href="">
-                  <img
-                    src={`http://127.0.0.1:8000/images/${food.image}`}
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div>
-                <div>{food.name}</div>
-                <div>{food.description}</div>
-                <div>
-                  {" "}
-                  <button type="button" className="btn btn-success">
-                    View more
-                  </button>
-                  <td>
-                    <Link to={"/quantity"}>
-                        View quantity
-                    </Link>
-                  </td>
-                </div>
-              </div>
-              <div>----------------------------------------------------</div>
-              <div>
-                <h4>{food.price}VND</h4>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div>No data for displaying</div>
-        )}
-        <div></div>
-      </div> */}
+      
       {/* header section starts     */}
       <header className="header fixed-top">
           <div className="container">
